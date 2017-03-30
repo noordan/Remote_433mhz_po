b7e6a280465e7c197f771a72f5e7d5429679e7a9
noordan <lukas.nord@hotmail.com>
2017-03-30 14:59:27 +0200

    merged from branch

--- backend/send_code.py
+++ backend/send_code.py
@@ -12,16 +12,43 @@ def connect():
     #print(recive.decode('ascii'))           # debug
     return connection, recive
 
+def current_time():
+    from datetime import datetime, timedelta
+    gmt = datetime.now() + timedelta(hours=0) # here can you change your time zone
+    # Save Hour and Minute to variable
+    gmt_hour = '{:%H}'.format(gmt)
+    gmt_minute = '{:%M}'.format(gmt)
+    return gmt_hour, gmt_minute
+
+def fetch_csv():
+    import csv
+    sockets = []
+    codes_csv = '/home/noordan/codes.csv' # The csv file, default ../codes.csv
+    with open(codes_csv, newline='',) as csvfile:
+        codereader = csv.DictReader(csvfile, delimiter=',')
+        for row in codereader:
+            sockets.append(row)
+    return sockets
+
 if __name__ == "__main__":
     import sys
-    try:
-        msg = sys.argv[1]
-        connection, recive = connect()
-        if recive.decode('ascii'):
-            send(connection, msg) #Send status
-            recive = connection.recv(1024)#Message recived
-            #print(recive.decode('ascii'))           # debug
-            connection.close()
-    except Exception as e:
-        #print(e)           # debug
-        pass
+    if (sys.argv[1] == "cron"):
+        hour, minute = current_time()
+        sockets = fetch_csv()
+        print(hour)
+        print(minute)
+        for socket in sockets:
+            print(socket['name'], socket['on_time'])
+
+    else:
+        try:
+            msg = sys.argv[1]
+            connection, recive = connect()
+            if recive.decode('ascii'):
+                send(connection, msg) #Send status
+                recive = connection.recv(1024)#Message recived
+                #print(recive.decode('ascii'))           # debug
+                connection.close()
+        except Exception as e:
+            #print(e)           # debug
+            pass
