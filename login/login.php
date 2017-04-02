59cf17d33794354c80ffef333460b980f0f7ab5c
noordan <lukas.nord@hotmail.com>
2017-04-02 13:58:20 +0200

    added custom css class

--- login/login.php
+++ login/login.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./custom.css">
+    <link rel="stylesheet" href="../custom.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
@@ -25,7 +25,7 @@ if (isset($_SESSION['username'])) {
         <input name="username" type="text" class="form-control" placeholder="Username" autofocus>
         <input name="password" type="password" class="form-control" placeholder="Password">
 
-        <button name="submit" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <button name="submit" class="btn btn-custom btn-lg btn-primary btn-block" type="submit" style="margin-top:10px;">Sign in</button>
 
         <?php
           if(isset($_SESSION['err'])){
